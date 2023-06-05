a3f5bd81d12331122b666962bd97bcb8d6fe18b0
0xonyx <jerdos-s@student.42lausanne.ch>
2023-06-05 15:39:48 +0200

    first commcit

---
+++ Colleen/c/Colleen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+# define string_program "#include <stdio.h>%1$c%1$c# define %2$c%3$s%1$c%2$c%1$c%1$cvoid	colleen()%1$c{%1$c	printf(string_program, 10, 34, string_program);%1$c}%1$c%1$c/* outside function */%1$cint	main(int argc, char **argv)%1$c{%1$c	//inside function%1$c	colleen();%1$c	return (0);%1$c}%1$c"
+
+void	colleen()
+{
+	printf(string_program, 10, 34, string_program);
+}
+
+/* outside function */
+int	main(int argc, char **argv)
+{
+	//inside function
+	colleen();
+	return (0);
+}

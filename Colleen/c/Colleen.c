969c39351088571ddcad5934dd7b0855f8e8e6ab
onyx <jeremy.dosg@gmail.com>
2023-06-05 17:47:58 +0200

    add asm

--- Colleen/c/Colleen.c
+++ Colleen/c/Colleen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-# define string_program "#include <stdio.h>%1$c%1$c# define %2$c%3$s%1$c%2$c%1$c%1$cvoid	colleen()%1$c{%1$c	printf(string_program, 10, 34, string_program);%1$c}%1$c%1$c/* outside function */%1$cint	main(int argc, char **argv)%1$c{%1$c	//inside function%1$c	colleen();%1$c	return (0);%1$c}%1$c"
+# define string_program "#include <stdio.h>%1$c%1$c# define string_program %2$c%3$s%2$c%1$c%1$cvoid	colleen()%1$c{%1$c	printf(string_program, 10, 34, string_program);%1$c}%1$c%1$c/* outside function */%1$cint	main(int argc, char **argv)%1$c{%1$c	//inside function%1$c	colleen();%1$c	return (0);%1$c}%1$c"
 
 void	colleen()
 {

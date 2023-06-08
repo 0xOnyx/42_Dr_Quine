2dbce05b5d3e34a580e420b131d65e413a169dc1
0xOnyx <jeremy.dosg@gmail.com>
2023-06-08 16:40:20 +0200

    add asm instruction

--- Colleen/asm/Colleen.asm
+++ Colleen/asm/Colleen.asm
@@ -1,18 +1,24 @@
+;comment outside
+global		main
+	section	.text
+	extern	printf
 
-global	main
-	section .text
-	extern printf
+colleen:
+	lea		rdi, [rel string_program]
+	mov		rsi, 10 ;1 \n
+	mov		rdx, 34 ;2 "
+	mov		rcx, 59 ;3 ;
+	mov		r8, rdi
+	mov 	al,0
+	call	printf wrt ..plt
+	ret
 
 main:
 	push	rbp
 	mov		rbp, rsp
-;	sub		rsp, 8  for VARIABLE LOCAL
+;	sub		rsp, 8  for VARIABLE LOCAL COMMENT
 
-	lea		rdi, [rel string_program]
-	mov		rsi, 10
-	mov		rdx, 34
-	mov		r10, rdi
-	call	printf wrt ..plt
+	jmp		colleen
 
 	mov		rax, 0
 	mov		rsp, rbp
@@ -20,5 +26,5 @@ main:
 	ret
 
 section .data
-    string_program: db "global start", 0x0
+    string_program: db "a%3$ca", 0x0A, 0x0
     len: equ $ - string_program

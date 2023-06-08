f474bb45ba964c46b9d47199e9c409d7a9a77d6a
onyx <jeremy.dosg@gmail.com>
2023-06-08 11:33:33 +0200

    add asm

---
+++ Colleen/asm/Colleen.asm
@@ -0,0 +1,24 @@
+
+global	main
+	section .text
+	extern printf
+
+m:
+	push	rbp
+	mov		rbp, rsp
+;	sub		rsp, 8  for VARIABLE LOCAL
+
+	lea		rdi, [rel string_program]
+	mov		rsi, 10
+	mov		rdx, 34
+	mov		r10, rdi
+	call	printf wrt ..plt
+
+	mov		rax, 0
+	mov		rsp, rbp
+	pop		rbp
+	ret
+
+section .data
+    string_program: db "global start", 0x0
+    len: equ $ - string_program

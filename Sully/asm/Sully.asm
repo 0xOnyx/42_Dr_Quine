84a7edc35a1108fd83eaf7f060f8dacb44659be2
0xOnyx <jeremy.dosg@gmail.com>
2023-06-12 20:52:02 +0200

    add some element

--- Sully/asm/Sully.asm
+++ Sully/asm/Sully.asm
@@ -1,12 +1,59 @@
+; +-------------------+  RSP
+; |                   |
+; |   src_file        |
+; |                   |
+; +-------------------+  RSP + 16
+; |                   |
+; |   exec_file       |
+; |                   |
+; +-------------------+  RSP + 32
+; |                   |
+; |   exec_cmd        |
+; |                   |
+; +-------------------+  RSP + 544
+; |                   |
+; |   fd              |
+; |                   |
+; +-------------------+  RSP + 552   /// 548 ?
+; |                   |
+; |   i               |
+; |                   |
+; +-------------------+  RBP
 
 extern dprintf
+extern sprintf
+extern system
+
 global	main
 
+	section .rodata
+src_file	db	__FILE__, 0x0
+sully_d		db	"Sully_%d.asm", 0x0
+sylly		db	"Sully_%d", 0x0
+compile_asm	db	"asm -felf64 %s -o %s", 0x0
+compile_ld	db	"gcc %s -o %s", 0x0
+exec_cmd	db	"./%s", 0x0
+
 	section .text
-
 main:
-	push	rbp
-	mov		rbp, rsp
-	sub		rsp, 1024
+	push		rbp
+	mov			rbp, rsp
+	sub			rsp, 1024
 
-	mov		[rsp]
+	mov	qword	[rsp + 552], 5
+	cmp	qwrod	[rsp + 552], 0
+	jnle		return
+
+	rel			rdi, [rel src_file]
+	rsi
+	rdx
+	rcx
+	r8
+	r9
+
+
+return :
+	mov		rax, 0
+	mov		rsp, rbp
+	pop		rbp
+	ret
--- Sully/asm/test.c
+++ Sully/asm/test.c
@@ -3,23 +3,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 int main(void){
 	char    src_file[10], exec_file[10], exec_cmd[0x1FF];
 	int     fd, i = v;
 	if (i <= 0){return (0);}
+
 	sprintf(src_file, "Sully_%d.c", i);
+
 	if (strcmp(__FILE__, src_file) == 0){i--;}
+
 	sprintf(src_file, "Sully_%d.c", i);
+
 	fd = open(src_file, O_WRONLY | O_CREAT | O_TRUNC , S_IRWXU | S_IRWXG | S_IROTH);
+
 	if (fd < 0)return (1);
+
 	dprintf(fd, string_programme, 10, 34, string_programme, i);
+
 	sprintf(exec_file, "Sully_%d", i);
+
 	sprintf(exec_cmd, "gcc %s -o %s", src_file, exec_file);
+
 	system(exec_cmd);
+
 	sprintf(exec_cmd, "./%s", exec_file);
+
 	system(exec_cmd);
+
 	close(fd);
 
 }
